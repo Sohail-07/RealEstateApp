ebae8b17e1eff33aebda75adbd57115b4f7e413a
sohail <emailtosohail7@gmail.com>
2023-01-19 15:26:54 +0530

    Services created

--- FontEnd/src/app/property/property-list/property-list.component.ts
+++ FontEnd/src/app/property/property-list/property-list.component.ts
@@ -1,41 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HousingService } from 'src/app/services/housing.service';
 
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
   styleUrls: ['./property-list.component.css']
 })
-export class PropertyListComponent {
-  properties: Array<any> = [
-    {
-      "Id": 1,
-      "Name": "Shaikh's Villa",
-      "Type": "House",
-      "Price": 12000000
-    },
-    {
-      "Id": 2,
-      "Name": "Syed's Villa",
-      "Type": "House",
-      "Price": 12000000
-    },
-    {
-      "Id": 3,
-      "Name": "Khan's Villa",
-      "Type": "House",
-      "Price": 12000000
-    },
-    {
-      "Id": 4,
-      "Name": "Mannat Villa",
-      "Type": "House",
-      "Price": 12000000
-    },
-    {
-      "Id": 5,
-      "Name": "Galexy Villa",
-      "Type": "House",
-      "Price": 12000000
-    }
-  ]
+export class PropertyListComponent implements OnInit{
+  
+  properties: any
+  
+  constructor(private housingService:HousingService){}
+  
+  ngOnInit():void{
+
+    this.housingService.getAllProperties().subscribe(
+      data=>{
+           this.properties = data
+      },
+      error => {
+        console.log(error)
+      }
+    );
+  }
+
 }
